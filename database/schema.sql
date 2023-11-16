c92fa051aa54841cf32e7fd70fa1b7e6ae3acc19
Cathy Garcia <garciacathy67@yahoo.com>
2023-11-16 21:22:42 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,56 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "orderDetails" (
+  "orderId" serial PRIMARY KEY,
+  "customerId" int,
+  "productId" int,
+  "quantity" int
+);
+
+CREATE TABLE "products" (
+  "productId" int PRIMARY KEY,
+  "name" text,
+  "brand" text,
+  "price" int,
+  "size" int,
+  "details" text,
+  "imageUrl" text,
+  "categoryId" int
+);
+
+CREATE TABLE "customer" (
+  "customerId" serial PRIMARY KEY,
+  "firstName" text,
+  "lastName" text,
+  "email" text,
+  "address" text
+);
+
+CREATE TABLE "cart" (
+  "customerId" serial PRIMARY KEY,
+  "quantity" int,
+  "productId" int
+);
+
+CREATE TABLE "images" (
+  "imagesId" serial PRIMARY KEY,
+  "productId" int,
+  "imageUrl" text
+);
+
+CREATE TABLE "category" (
+  "categoryId" serial PRIMARY KEY,
+  "name" text
+);
+
+ALTER TABLE "orderDetails" ADD FOREIGN KEY ("customerId") REFERENCES "customer" ("customerId");
+
+ALTER TABLE "orderDetails" ADD FOREIGN KEY ("productId") REFERENCES "products" ("productId");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("categoryId") REFERENCES "category" ("categoryId");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("productId") REFERENCES "products" ("productId");
+
+ALTER TABLE "images" ADD FOREIGN KEY ("productId") REFERENCES "products" ("productId");
